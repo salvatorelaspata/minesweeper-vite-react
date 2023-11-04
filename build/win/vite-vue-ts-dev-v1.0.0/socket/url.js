99e1b9e8a1b023e5bfcba4e94bcd8084edbe8eef
salvatorelaspata <salvatore.la.spata@gmail.com>
2023-11-04 21:22:20 +0100

    remove build

--- build/win/vite-vue-ts-dev-v1.0.0/socket/url.js
+++
@@ -1,7 +0,0 @@
-/**
- * @notice This is a rexports of `url/index.js` so consumers will
- * need to only `import * as url from 'socket:url'`
- */
-import * as exports from './url/index.js'
-export * from './url/index.js'
-export default exports
